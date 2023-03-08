a245a323cbf8a8f63bbf0fdefdade4b763e1a4ae
Francesco <fleuzzi97@gmail.com>
2023-03-08 22:54:29 +0100

    due tabs, 3 pagine, ion-segment non funzionante

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,8 +20,12 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
