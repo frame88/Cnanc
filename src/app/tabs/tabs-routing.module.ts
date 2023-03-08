7911c0ae7bc96d791cd71a17e246016594eb21c1
Francesco <fleuzzi97@gmail.com>
2023-03-08 23:19:51 +0100

    sostituzione di home con tabs3, in questo modo i tabs sono nativamente presenti

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,10 +20,6 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-        path: 'home',
-        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
-      },
-      {
         path: '',
         redirectTo: '/tabs/home',
         pathMatch: 'full'
